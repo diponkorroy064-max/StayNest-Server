6c478249818c00a4f783a330f86e2c5ae6968d60
diponkorroy064-max <diponkorroy064@gmail.com>
2026-06-24 07:33:32 +0600

    add 3 favourites api for posting , deleting and email based getting data

--- index.js
+++ index.js
@@ -18,14 +18,16 @@ const client = new MongoClient(uri, {
   }
 });
 
+
 async function run() {
   try {
     await client.connect();
     const database = client.db("staynest");
 
     const propertyCollections = database.collection('properties');
+    const favouritesCollection = database.collection('favourites');
 
-    //api---
+    //properties api---
     app.post('/api/properties', async (req, res) => {
       const properties = req.body;
       // console.log(properties);
@@ -42,20 +44,123 @@ async function run() {
     app.get('/api/properties/:id', async (req, res) => {
       try {
         const id = req.params.id;
-        console.log("RAW ID:", id);
+        // console.log("RAW ID:", id);
 
         const query = { _id: new ObjectId(id) };
         const result = await propertyCollections.findOne(query);
         res.json(result || null);
 
-      } catch (error) {
-        console.log("BACKEND ERROR:", error.message);
-
+      }
+      catch (error) {
+        // console.log("BACKEND ERROR:", error.message);
         res.status(500).json({
           message: "Invalid ObjectId or server error"
         });
       }
     });
+
+    app.get("/properties/byEmail", async (req, res) => {
+      try {
+        const email = req.query.email;
+
+        if (!email) {
+          return res.status(400).json({
+            message: "Email query is required",
+          });
+        }
+        const query = { ownerEmail: email };
+        const result = await propertyCollections.find(query).toArray();
+
+        res.status(200).json(result);
+      }
+      catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    });
+
+
+    // Favourites api---
+    app.post('/api/favourites', async (req, res) => {
+      try {
+        const favourites = req.body;
+        // console.log(favourites);
+        const exists = await favouritesCollection.findOne({
+          propertyId: favourites.propertyId,
+        });
+
+        if (exists) {
+          return res.status(409).json({
+            message: "Property already exists in your favourites.",
+          });
+        }
+
+        const result = await favouritesCollection.insertOne(favourites);
+        res.status(201).json(result);
+      }
+      catch (err) {
+        res.status(500).json({ message: err.message })
+      }
+    })
+
+
+    app.get('/api/favourites/byEmail', async (req, res) => {
+      try {
+        const email = req.query.email;
+
+        if (!email) {
+          return res.status(400).json({
+            message: "Email query is required",
+          });
+        }
+        const query = { currentUserEmail: email };
+        const result = await favouritesCollection.find(query).toArray();
+        res.status(200).json(result);
+      }
+      catch (error) {
+        res.status(500).json({
+          message: error.message,
+        });
+      }
+    })
+
+    app.delete("/api/favourites/:id", async (req, res) => {
+      console.log(req.params.id);
+
+      try {
+        const { id } = req.params;
+
+        const result = await favouritesCollection.deleteOne({
+          _id: id,
+        });
+        console.log(result);
+        
+        if (result.deletedCount === 0) {
+          return res.status(404).json({
+            message: "Favourite not found.",
+          });
+        }
+
+        res.json({
+          message: "Favorite removed successfully.",
+        });
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({
+          message: err.message,
+        });
+      }
+    });
+
+
+
+
+
+
+
+
+
 
 
     // Send a ping to confirm a successful connection----
